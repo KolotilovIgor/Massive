fade1b351a807d31559e7579d76b49a3a7a47b1a
IgorK <gosha424@mail.ru>
2024-04-14 21:26:40 +0300

    Добавьте файлы проекта.

---
+++ Massive/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace Massive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] a = { { 7, 3, 2 }, { 4, 9, 6 }, { 1, 8, 5 } };
+            int totalElements = a.GetLength(0) * a.GetLength(1);
+            int[] flattenedArray = new int[totalElements];
+            int index = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    flattenedArray[index++] = a[i, j];
+                }
+            }
+            Array.Sort(flattenedArray);
+            index = 0;
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    a[i, j] = flattenedArray[index++];
+                }
+            }
+            for (int i = 0; i < a.GetLength(0); i++)
+            {
+                for (int j = 0; j < a.GetLength(1); j++)
+                {
+                    Console.Write(a[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
